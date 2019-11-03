f22ccd8d01fee4c018a8877c9e857b67f7ee309a
mohammedkah@outlook.com <mohammedkah@outlook.com>
2019-11-03 15:36:38 +0100

    HI

---
+++ Min_Project/view/windows/login.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Min_Project.view.windows
+{
+    /// <summary>
+    /// Interaction logic for login.xaml
+    /// </summary>
+    public partial class login : Window
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void ResizeAction(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        private void MinAction(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void PowerAction(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+    }
+}
